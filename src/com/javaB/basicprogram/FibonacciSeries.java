85f557a6bcf94a7252721b7e53b021d0141b0887
Loganathan R <prk.logu@gmail.com>
2022-06-06 17:48:05 +0530

    FibonacciSeries recursion

--- src/com/javaB/basicprogram/FibonacciSeries.java
+++ src/com/javaB/basicprogram/FibonacciSeries.java
@@ -1,14 +1,31 @@
 package com.javaB.basicprogram;
 
 public class FibonacciSeries {
-    public static void main(String ...arg){
-        int n1=0, n2=1, n3, count=10;
-        System.out.print(n1+" "+n2);
-        for(int i=2; i<=10;i++){
+    static  int n1=0, n2=1, n3;
+    public static void main(String... arg) {
+       int count=10;
+       System.out.print(n1 + " " + n2);
+        fibonacci(count-2);
+        //fibonacciRecursion(count-2);
+    }
+
+    public static void fibonacci(int count) {
+
+        for (int i = 2; i <= count; i++) {
+            n3 = n1 + n2;
+            System.out.print(" " + n3);
+            n1 = n2;
+            n2 = n3;
+        }
+    }
+
+    public static void fibonacciRecursion(int count){
+        if(count>0){
             n3= n1+n2;
-            System.out.print(" "+n3);
+            System.out.print(" " + n3);
             n1=n2;
             n2=n3;
+            fibonacciRecursion(count-1);
         }
     }
 }
